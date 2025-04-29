0acc6c0fcb7888df09094cf8677283813c0310bd
Shaanrahman123 <coderraufur@gmail.com>
2025-04-29 21:51:00 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     historyApiFallback: true,
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
 });
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   base: './',
+//   plugins: [react()],
+// });
