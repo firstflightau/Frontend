8eee786e1d1d1ca4607bf7e993041c776bd80b69
firstflightau <firstflight.au@gmail.com>
2025-04-29 22:24:22 +0530

    Update Constant.js

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -1,5 +1,5 @@
 export const apiURL = {
   // baseURL: "https://ffbackend-sn85.onrender.com",
-  baseURL: "https://backend-ll8i.onrender.com/" //live production
+  baseURL: "https://backend-ll8i.onrender.com" //live production
   // baseURL: "http://localhost:8000",
 };
