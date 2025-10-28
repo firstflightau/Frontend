89bd85b2e9e74d0fec09a817fa391e92b59cf643
Shaanrahman123 <coderraufur@gmail.com>
2025-10-29 00:06:09 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,13 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
   },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   base: './',
-//   plugins: [react()],
-// });
